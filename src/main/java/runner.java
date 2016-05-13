8e89707b71abc75235a1f77db1d3597ccec9d64f
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-05-13 11:23:27 -0500

    Move cobspec route setup to its own class

--- src/main/java/runner.java
+++ src/main/java/runner.java
@@ -3,20 +3,7 @@ public class runner {
     public static void main(String [] args) {
         try {
             ArgsParser parser = new ArgsParser(args);
-            Router router = new Router();
-            router.addRoute("GET /", new Response());
-            router.addRoute("PUT /form", new Response());
-            router.addRoute("POST /form", new Response());
-            Response methodOptions = new Response();
-            methodOptions.addHeader("Allow", "GET,HEAD,POST,OPTIONS,PUT");
-            methodOptions.addHeader("Content-Length", "0");
-            router.addRoute("GET /method_options", methodOptions);
-            router.addRoute("PUT /method_options", methodOptions);
-            router.addRoute("POST /method_options", methodOptions);
-            router.addRoute("HEAD /method_options", methodOptions);
-            router.addRoute("OPTIONS /method_options",methodOptions);
-//            System.out.println(router.getResponse("GET /method_options").buildResponse());
-
+            Router router = new CobspecSetup().buildRoutes();
             Server server = new Server(router);
             System.out.println("Starting Server on port " + parser.getPort() );
             server.start(parser.getPort());
