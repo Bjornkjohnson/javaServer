748e456730212cc8a99ef2e314ce1d326782acda
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-05-19 12:08:00 -0400

    Add headers and parsing to requests

--- src/main/java/bjohnson/RequestBuilder.java
+++ src/main/java/bjohnson/RequestBuilder.java
@@ -1,22 +1,34 @@
 package bjohnson;
 
 import java.io.BufferedReader;
+import java.util.HashMap;
 
 public class RequestBuilder {
     private BufferedReader in;
     private Request request;
+    private HashMap<String, String> headers;
 
 
     public RequestBuilder(BufferedReader in) throws Exception{
         this.in = in;
         request = new Request();
-
+        headers = new HashMap<>();
     }
 
     public Request buildRequest() throws Exception {
         String rawRequest = in.readLine();
+        String header;
+        while ((header = in.readLine()).length() != 0){
+            addHeader(header);
+        }
+        request.setHeaders(headers);
         buildRequestLine(rawRequest);
         return request;
+    }
+
+    private void addHeader(String header) {
+        String[] parsedHeader = header.split(": ");
+        headers.put(parsedHeader[0], parsedHeader[1]);
     }
 
     private void buildRequestLine(String rawRequest){
