481fd61e32b083e25330c0a955defc6d3183927d
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-05-22 15:49:52 -0400

    Add echo params response builder

---
+++ src/test/java/bjohnson/ResponseHandlers/EchoParamsResponseBuilder.java
@@ -0,0 +1,19 @@
+package bjohnson.ResponseHandlers;
+
+import bjohnson.Request;
+
+public class EchoParamsResponseBuilder implements ResponseBuilderInterface {
+    Response response;
+    @Override
+    public Response getResponse(Request request) {
+        response = new Response();
+        buildResponseBody(request);
+        return response;
+    }
+
+    private void buildResponseBody(Request request) {
+        String[] params = request.getParams();
+        String paramsString = String.join("\r\n", params);
+        response.setBody(paramsString.getBytes());
+    }
+}
